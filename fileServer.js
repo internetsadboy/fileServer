c1acb0a2a74a0b98dbeb36e98f1ca3abb4765b86
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-03 21:34:00 -0800

    Update fileServer.js

--- fileServer.js
+++ fileServer.js
@@ -1,27 +1,27 @@
 module.exports = function(file, req, res) {
-	var fs = require('fs')
-		, ext = require('path').extname(file)
-		, type = ''
-		, fileExtensions = {
-				'html':'text/html',
-				'css':'text/css',
-				'js':'text/javascript',
-				'json':'application/json',
-				'jpg':'image/jpg',
-				'png':'image/png',
-				'wav':'audio/wav'
-			};
-	for(var i in fileExtensions) {
-		if(ext === i) {
-			type = fileExtensions[i];
-			break;
-		}
-	}
-	fs.exists(file, function(exists) {
-		if(exists) {
-			res.writeHead(200, {'content-type':type});
-			fs.createReadStream(file).pipe(res);
-		} else console.log(file, 'file dne');
-	});
+  var fs = require('fs')
+    , ext = require('path').extname(file)
+    , type = ''
+    , fileExtensions = {
+        'html':'text/html',
+        'css':'text/css',
+        'js':'text/javascript',
+        'json':'application/json',
+        'jpg':'image/jpg',
+        'png':'image/png',
+        'wav':'audio/wav'
+      };
+  for(var i in fileExtensions) {
+    if(ext === i) {
+      type = fileExtensions[i];
+      break;
+    }
+  }
+  fs.exists(file, function(exists) {
+    if(exists) {
+      res.writeHead(200, {'content-type':type});
+      fs.createReadStream(file).pipe(res);
+    } else console.log(file, 'file dne');
+  });
 }
 
